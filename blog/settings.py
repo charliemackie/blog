81508f96cff5a14d49391ffa30b21d130a432474
charliemackie <cmacki43@uwo.ca>
2021-05-07 17:46:16 -0400

    static update

--- blog/settings.py
+++ blog/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/posts/static/'
 PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
